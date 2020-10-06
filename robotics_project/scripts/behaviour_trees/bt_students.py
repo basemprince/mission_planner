6a487f3118f221649901872949961a4f1cde0f8a
Basem Shaker <bshaker@kth.se>
2020-10-06 13:19:38 +0200

    partc2

--- robotics_project/scripts/behaviour_trees/bt_students.py
+++ robotics_project/scripts/behaviour_trees/bt_students.py
@@ -11,6 +11,7 @@ from actionlib import SimpleActionClient
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from robotics_project.srv import MoveHead, MoveHeadRequest, MoveHeadResponse
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import tf, math
 
 import functools
@@ -18,8 +19,57 @@ import functools
 
 # remember current cycle (for part A)
 start_over_handler = 1
-
+kidnapped = True
+kidnap_feedback = False
+kidnap_feedback1 = False
 cube_pose = None
+poseCov = PoseWithCovarianceStamped()
+
+def poseCallBack (data):
+	global poseCov
+	poseCov = data
+
+
+class check_kidnap(pt.behaviour.Behaviour):
+
+	"""
+	Sends a goal to the tuck arm action server.
+	Returns running whilst awaiting the result,
+	success if the action was succesful, and v.v..
+	"""
+
+	# get global variable (target of callback)
+
+	def __init__(self, param=""):
+		# get param
+		self.param = param
+		# set subscriber
+		self.kidnap_msg = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped,poseCallBack)
+		self.x_warn = 0.2
+		self.y_warn = 0.2
+		self.yaw_warn = 0.1
+		# become a behaviour
+		super(check_kidnap, self).__init__("check kidnap "+param+"!")
+
+
+	def update(self):
+		global poseCov
+		global kidnapped
+		global kidnap_feedback
+		std_x = math.sqrt(poseCov.pose.covariance[6*0+0])
+		std_y = math.sqrt(poseCov.pose.covariance[6*1+1])
+		std_yaw = math.sqrt(poseCov.pose.covariance[6*5+5])
+
+		if (std_x > self.x_warn or std_y > self.y_warn or std_yaw > self.yaw_warn):
+			kidnapped = True
+			return pt.common.Status.FAILURE
+		else:
+			kidnapped = False
+			return pt.common.Status.SUCCESS
+
+
+
+
 def cube_pose_cb(data):
 	global cube_pose
 	cube_pose = data
@@ -83,8 +133,9 @@ class counter(pt.behaviour.Behaviour):
 		super(counter, self).__init__(name)
 
 	def update(self):
-
-		if self.start_over_count < start_over_handler:
+		global kidnapped
+		global kidnap_feedback
+		if self.start_over_count < start_over_handler or (kidnapped and kidnap_feedback):
 			self.i = 0
 			self.done = False
 			self.start_over_count += 1
@@ -97,8 +148,10 @@ class counter(pt.behaviour.Behaviour):
 			self.done = True
 		
 		if self.done:
+			kidnap_feedback = True
 			return pt.common.Status.SUCCESS
 		else:
+			kidnap_feedback = False
 			return pt.common.Status.FAILURE
 
 
@@ -455,11 +508,20 @@ class global_localization(pt.behaviour.Behaviour):
 		super(global_localization, self).__init__("global localization!")
 
 	def update(self):
-
+		global kidnap_feedback1
+		global kidnapped
 		if self.start_over_count < start_over_handler:
 			self.done = False
 			self.tried = False
 			self.start_over_count += 1
+
+
+		if  kidnapped and not kidnap_feedback1:
+			self.tried = False
+			self.done = False
+			kidnap_feedback1 = True
+
+
 
 		if not self.tried:
 			self.tried = True
@@ -469,6 +531,7 @@ class global_localization(pt.behaviour.Behaviour):
 		# if succesful
 		else:
 			self.done = True
+			kidnap_feedback1 = False
 			return pt.common.Status.SUCCESS
 
 
@@ -530,7 +593,7 @@ class goto(pt.behaviour.Behaviour):
 				self.goal_pub.publish(self.table2_msg)
 
 			self.tried = True
-			return pt.common.Status.RUNNING
+			return pt.common.Status.FAILURE
 
 		# if succesful
 		else:
@@ -667,6 +730,7 @@ class BehaviourTree(ptr.trees.BehaviourTree):
 
 		rospy.loginfo("Initialising behaviour tree")
 		
+		b00 = check_kidnap()
 
 		# tuck the arm
 		b0 = tuckarm()
@@ -693,6 +757,11 @@ class BehaviourTree(ptr.trees.BehaviourTree):
 			children=[counter(20, "Waited?"), go("Wait!", 0, 0)]
 		)
 
+
+		b004 = pt.composites.Sequence(
+			name = "initalize location",
+			children= [b01,b02,b03,b04,b00]
+		)
 
 		# go to cube
 		b05 = goto_action("table1")
@@ -737,120 +806,8 @@ class BehaviourTree(ptr.trees.BehaviourTree):
 		# is cube there? ...
 
 
-
-		'''
-		# lower head
-		b1 = movehead("down")
-
-
-		# detect cube
-		b2 = detect_cube("wait_for_detection")
-
-
-		# pick up cube when detected
-		b3 = pickplace_cube("pick")
-
-
-
-		# turn 180
-		b4 = pt.composites.Selector(
-			name="Turn 180 fallback",
-			children=[counter(31, "Turned?"), go("Turn!", 0, -1)]
-		)
-		# wait a bit
-		b5 = pt.composites.Selector(
-			name="Wait fallback",
-			children=[counter(18, "Waited?"), go("Wait!", 0, 0)]
-		)
-		# move strait to other table
-		b6 = pt.composites.Selector(
-			name="Go to table fallback",
-			children=[counter(15, "At table?"), go("Go to table!", 0.5, 0)]
-		)
-		# go to other table
-		b456 = pt.composites.Sequence(
-			name="Go to other table sequence",
-			children=[b4, b5, b6]
-		)
-
-
-
-		# place the cube
-		b7 = pickplace_cube("place")
-
-
-		# tuck the arm
-		b8 = tuckarm()
-
-
-
-		# turn 180
-		b42 = pt.composites.Selector(
-			name="Turn 180 fallback",
-			children=[counter(31, "Turned?"), go("Turn!", 0, -1)]
-		)
-		# wait a bit
-		b52 = pt.composites.Selector(
-			name="Wait fallback",
-			children=[counter(18, "Waited?"), go("Wait!", 0, 0)]
-		)
-		# move strait to other table
-		b62 = pt.composites.Selector(
-			name="Go to table fallback",
-			children=[counter(15, "At table?"), go("Go to table!", 0.5, 0)]
-		)
-		# go to other table
-		b4562 = pt.composites.Sequence(
-			name="Go to other table sequence",
-			children=[b42, b52, b62]
-		)
-
-
-
-		# check if cube is placed
-		b9 = pt.composites.Selector(
-			name="Go to table fallback",
-			children=[detect_cube("once"), b4562]
-		)
-
-
-		# be happy
-		b10 = pt.composites.Selector(
-			name="Happy fallback",
-			children=[counter(20, "Happy?"), go("Happy!", 0, 1)]
-		)
-		'''
-
-		'''
-
-		# go to door until at door
-		b0 = pt.composites.Selector(
-			name="Go to door fallback", 
-			children=[counter(30, "At door?"), go("Go to door!", 1, 0)]
-		)
-
-		# tuck the arm
-		b1 = tuckarm()
-
-		# go to table
-		b2 = pt.composites.Selector(
-			name="Go to table fallback",
-			children=[counter(5, "At table?"), go("Go to table!", 0, -1)]
-		)
-
-		# move to chair
-		b3 = pt.composites.Selector(
-			name="Go to chair fallback",
-			children=[counter(13, "At chair?"), go("Go to chair!", 1, 0)]
-		)
-
-		# lower head
-		b4 = movehead("down")
-
-		'''
-
 		# become the tree
-		tree = RSequence(name="Main sequence", children=[b0, b01, b02, b03, b04, b05, b06, b07, b08, b09, b10, b11, b12])
+		tree = RSequence(name="Main sequence", children=[b004, b0, b05, b06, b07, b08, b09, b10, b11, b12])
 		super(BehaviourTree, self).__init__(tree)
 
 
